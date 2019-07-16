1c2982dcdce94cc6ec13f98486cf3cff3f45cf19
AmariEllington <amari.l.ellington@gmail.com>
2019-07-16 17:26:11 +0100

    tuesday finish

--- sportcast-frontend/src/Containers/Twitter.js
+++ sportcast-frontend/src/Containers/Twitter.js
@@ -2,11 +2,28 @@ import React, { Component } from "react";
 import { TwitterTimelineEmbed } from "react-twitter-embed";
 
 export default class Twitter extends Component {
+  state = {
+    input: ""
+  };
+
+  handleClick = event => {
+    event.preventDefault();
+    this.setState({
+      input: event.target.formInput.value
+    });
+    console.log(event.target.formInput.value);
+  };
   render() {
     return (
       <div className="twitter card">
+        <form onSubmit={this.handleClick}>
+          <input type="text" name="formInput" />
+          <input type="submit" value="submit" />
+        </form>
         <TwitterTimelineEmbed sourceType="profile" screenName="espn" />
       </div>
     );
   }
 }
+
+// update state and twitter

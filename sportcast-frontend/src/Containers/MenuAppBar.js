be023e435430da62d27cb01a04ae6984f1a26e9d
AmariEllington <amari.l.ellington@gmail.com>
2019-07-22 14:34:08 +0100

    modal added

--- sportcast-frontend/src/Containers/MenuAppBar.js
+++ sportcast-frontend/src/Containers/MenuAppBar.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
-import { Menu, Segment } from "semantic-ui-react";
+import { Menu, Segment, Button, Image, Modal, Header } from "semantic-ui-react";
 
 export default class MenuExampleInvertedSegment extends Component {
+  state = { open: false };
+
+  show = dimmer => () => this.setState({ dimmer, open: true });
+  close = () => this.setState({ open: false });
+
   render() {
+    const { open, dimmer } = this.state;
     return (
       <Segment inverted>
         <Menu inverted secondary>
@@ -16,6 +22,25 @@ export default class MenuExampleInvertedSegment extends Component {
               <i class="hand peace icon" />
             </div>
           </div>
+
+          <Button onClick={this.show("blurring")}>Blurring</Button>
+
+          <Modal dimmer={dimmer} open={open} onClose={this.close}>
+            <Modal.Header>Preference</Modal.Header>
+            <Modal.Content />
+            <Modal.Actions>
+              <Button color="black" onClick={this.close}>
+                Close
+              </Button>
+              <Button
+                positive
+                icon="checkmark"
+                labelPosition="right"
+                content="Set"
+                onClick={this.close}
+              />
+            </Modal.Actions>
+          </Modal>
         </Menu>
       </Segment>
     );
--- sportcast-frontend/src/Containers/Spotify.js
+++ sportcast-frontend/src/Containers/Spotify.js
@@ -23,7 +23,33 @@ export default class Spotify extends Component {
     return (
       <div className="card-item">
         <iframe
-          src="https://www.mixcloud.com/widget/iframe/?light=1&feed=%2Fguardianfootballweekly%2F"
+          width="100%"
+          height="60"
+          src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&light=1&feed=%2Fguardianfootballweekly%2F"
+          frameborder="0"
+        />
+        <iframe
+          width="100%"
+          height="60"
+          src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&light=1&feed=%2Ffoxsportsfootballshow%2F"
+          frameborder="0"
+        />
+        <iframe
+          width="100%"
+          height="60"
+          src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&light=1&feed=%2FESPNFantasyFocusFootball%2F"
+          frameborder="0"
+        />
+        <iframe
+          width="100%"
+          height="60"
+          src="https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&light=1&feed=%2Ffoxcricketpodcast-foxsportsaus%2F"
+          frameborder="0"
+        />
+        <iframe
+          width="100%"
+          height="60"
+          src="https://open.spotify.com/show/1RDjh3ix2iXvatREVd4VLR"
           frameborder="0"
         />
       </div>
